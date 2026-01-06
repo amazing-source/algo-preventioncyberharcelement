a1f1c1ac26146b848065746994568eea7b65eee9
amazing-source <dilandjematene@gmail.com>
2026-01-06 16:58:22 +0100

    Delete projetcyberharcelement directory

--- projetcyberharcelement/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define API_KEY "f0FYuZ5DRIMNbBQNZSJTHASaURCXHvoD"
-#define API_URL "https://api.mistral.ai/v1/chat/completions"
-#define MODEL "mistral-small-latest"
-#define MAX_TOKENS 2048
-
-#endif
